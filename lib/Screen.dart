268291480cb50a2c97dcda25f2a9e33e8e0b0879
Yasantha Ayesh <49565616+yasa1995@users.noreply.github.com>
2020-09-02 18:25:17 +0530

    Initial commit

---
+++ lib/Screen.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_pro/carousel_pro.dart';
+
+class SecondPage extends StatefulWidget {
+  @override
+  _SecondPageState createState() => _SecondPageState();
+}
+
+class _SecondPageState extends State<SecondPage> {
+  @override
+  Widget build(BuildContext context) {
+    Widget ImageCarousel = new Container(
+      height: 200.0,
+      child: Carousel(
+        boxFit: BoxFit.cover,
+        images: [
+          AssetImage("assets/images/youSketch1.jpg"),
+          AssetImage("assets/images/youSketch2.jpg"),
+          AssetImage("assets/images/youSketch3.jpg"),
+          AssetImage("assets/images/youSketch4.jpg"),
+        ],
+        autoplay: true,
+        animationCurve: Curves.fastOutSlowIn,
+        animationDuration: Duration(microseconds: 1000),
+        dotColor: Color(0xFFBD34C59),
+        dotSize: 4,
+      ),
+    );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("U Sketch"),
+        backgroundColor: Color(0xFFBD34C59),
+        actions: [
+          new IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+              onPressed: null),
+          new IconButton(
+              icon: Icon(
+                Icons.shopping_cart,
+                color: Colors.white,
+              ),
+              onPressed: null)
+        ],
+      ),
+      body: ListView(
+        children: [
+          ImageCarousel,
+          //Categories List
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text("Categories"),
+          )
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text("Ravindu"),
+              accountEmail: Text("ravindujayakodi71@gmail.com"),
+              currentAccountPicture: GestureDetector(
+                child: CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  child: Icon(
+                    Icons.person,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: Color(0xFFBD34C59),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text("Home Page"),
+                leading: Icon(
+                  Icons.home,
+                  color: Color(0xFFBD34C59),
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text("My Account"),
+                leading: Icon(
+                  Icons.person,
+                  color: Color(0xFFBD34C59),
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text("My orders"),
+                leading: Icon(
+                  Icons.shopping_basket,
+                  color: Color(0xFFBD34C59),
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text("About"),
+                leading: Icon(
+                  Icons.help,
+                  color: Color(0xFFBD34C59),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
